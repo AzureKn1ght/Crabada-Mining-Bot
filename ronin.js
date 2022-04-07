976fff7773282a00223bdc047a85aa08d48a8205
AzureKn1ght <philsonnah@msn.com>
2022-04-07 21:41:55 +0800

    Ronin Restake Done

--- ronin.js
+++ ronin.js
@@ -1,3 +1,4 @@
+const scheduler = require("node-schedule");
 const { ethers } = require("ethers");
 require("dotenv").config();
 
@@ -15,20 +16,23 @@ const provider = new ethers.getDefaultProvider(
   })
 );
 
+// Staking Contract ABI
 const stakingABI = [
   "function restakeRewards()",
   "function claimPendingRewards()",
   "function unstakeAll()",
 ];
 
+// Setup wallet and contract connections
 const contractAddress = "0x05b0bb3c1c320b280501b86706c3551995bc8571";
 const contract = new ethers.Contract(contractAddress, stakingABI, provider);
 const wallet = new ethers.Wallet(PRIV_KEY, provider);
 const connectedContract = contract.connect(wallet);
 
+// Main Function
 const main = async () => {
   try {
-    // log ronin balance
+    // current ronin balance
     const balance = await provider.getBalance(WALLET_ADDRESS);
     console.log("RON Balance: " + ethers.utils.formatEther(balance));
 
@@ -37,41 +41,52 @@ const main = async () => {
     console.log("Gas Price: " + ethers.utils.formatEther(gas));
 
     // first restake on launch
-    const successful = await restake();
+    restake();
 
-    // restake successful, set schedule
-    if (successful) {
-      console.log("Restake Done!");
-    }
+    // continues until stopped
   } catch (error) {
     console.error(error);
   }
 };
 
+// Restake Function
 const restake = async () => {
   try {
+    // set random gasLimit to avoid detection
     const randomGas = 400000 + (Math.random() * (99999 - 10000) + 10000);
     const overrideOptions = {
       gasLimit: Math.floor(randomGas),
     };
 
-    // to be tested
+    // execute the restaking transaction
     const restake = await connectedContract.restakeRewards(overrideOptions);
     const receipt = await restake.wait();
 
+    // wait for transaction to complete
     if (receipt) {
-      console.log("Restake successful");
-      console.log(restake);
-      console.log(receipt);
-
-      return true;
+      // restake successful schedule next
+      console.log("RESTAKE SUCCESSFUL");
+      scheduleNext(new Date());
+      return;
     }
   } catch (error) {
+    // restake failed try again tomorrow
     console.error(error);
-    return false;
+    scheduleNext(new Date());
+    return;
   }
+};
 
-  return false;
+// Job Scheduler Function
+const scheduleNext = async (nextDate) => {
+  // set the next restake time (24hrs, 1min, 30sec from now)
+  nextDate.setHours(nextDate.getHours() + 24);
+  nextDate.setMinutes(nextDate.getMinutes() + 1);
+  nextDate.setSeconds(nextDate.getSeconds() + 30); //test just the seconds tmr
+  console.log("Next Restake: " + nextDate);
+
+  // schedule next restake
+  scheduler.scheduleJob(nextDate, restake);
 };
 
 main();
